fe08b9b67f571b3f53fa7b8473ecaa9fb8b1aa31
Saradha-6874 <ssmsaradha@gmail.com>
2026-01-31 15:27:33 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class marks{
+    public static void main(String args[])
+    {
+         System.out.println("enter your marks:");
+        Scanner sc=new Scanner(System.in);
+        int marks=sc.nextInt();
+        System.out.println(marks);
+    }
+}
