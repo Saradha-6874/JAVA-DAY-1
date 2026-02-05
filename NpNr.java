2b7adcd164ffd49c55cabbdbb24dfec76f05344c
Saradha-6874 <ssmsaradha@gmail.com>
2026-02-05 10:52:30 +0530

    Add files via upload

---
+++ NpNr.java
@@ -0,0 +1,11 @@
+public class NpNr{
+    public static void main(String[] args) {
+        add ();
+    }
+    static void add(){
+        sub();
+    }
+    static void sub()
+    {System.out.println("sub method");
+    }
+    }
