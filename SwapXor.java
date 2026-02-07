ae56c9110b60fab2b0df474122bca357c2da2da3
Saradha-6874 <ssmsaradha@gmail.com>
2026-02-07 15:22:01 +0530

    Add files via upload

---
+++ SwapXor.java
@@ -0,0 +1,11 @@
+public class SwapXor{
+    public static void main(String args[]){
+        int x=50;
+        int y=60;
+        x=x^y;
+        y=x^y;
+        x=x^y;
+        System.out.println("x:"+x);
+        System.out.println("y"+y);
+    }
+}
