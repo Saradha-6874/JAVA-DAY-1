a4247fbb664f4029177fbab6e1d2412372e6f6be
Saradha-6874 <ssmsaradha@gmail.com>
2026-02-07 11:39:03 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,16 @@
+public class Primitive{
+    public static void main(String[]args){
+     String s="latha";
+     int a=18;
+     char ch='A';
+     double d=99;
+     boolean result=true;
+    System.out.println("student name:"+s);
+    System.out.println("age:"+a);
+    System.out.println("grade:"+ch);
+    System.out.println("marks:"+d);
+    System.out.println("status:"+result);
+    }
+}
+    
+    
