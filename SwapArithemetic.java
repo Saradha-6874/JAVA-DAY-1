2be0f785efa6929ec82b29761ef929d64411db1c
Saradha-6874 <ssmsaradha@gmail.com>
2026-02-07 15:21:30 +0530

    Add files via upload

---
+++ SwapArithemetic.java
@@ -0,0 +1,11 @@
+public class SwapArithemetic{
+    public static void main(String args[]){
+        int x=50;
+        int y=60;
+        x=x+y;
+        y=x-y;
+        x=x-y;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
